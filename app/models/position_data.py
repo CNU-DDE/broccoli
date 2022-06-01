69e2f8615fc1da9f0cac7d2cb20ebb8e15760f88
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-01 23:24:11 +0900

    feat: additional field for position_data

--- app/models/position_data.py
+++ app/models/position_data.py
@@ -2,4 +2,17 @@ from django.db import models
 from .base_data import BaseData
 
 class PositionData(BaseData, models.Model):
-    pass
+
+    employment_period = models.CharField(
+        max_length = 255,
+    )
+
+    working_time = models.CharField(
+        max_length = 255,
+    )
+
+    payment_interval_type = models.SmallIntegerField()
+
+    payment_per_internal = models.IntegerField()
+
+    hiring_number = models.IntegerField()
