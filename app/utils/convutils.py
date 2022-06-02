493de92311546ea8e4fbd85b68a0df66f0e63aea
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-03 00:05:29 +0900

    feat: API endpoint

--- app/utils/convutils.py
+++ app/utils/convutils.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 def user_type(is_employee):
-    runmode = 0 if settings.RUNMODE is "production" else 2
+    runmode = 0 if settings.RUNMODE == "production" else 2
     return runmode + (1 if is_employee else 0)
 
 def error_message(err):
---
+++ app/views/user_token_response.py
@@ -0,0 +1,55 @@
+from ..models import User
+from ..utils import hashutils, convutils
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+"""
+[POST] /api/user
+@RequestBody: {
+    password:   string,
+    keystore:   { did: string, walletAddress: string, privKey: string, pubKey: string }
+}
+"""
+class UserTokenResponse(APIView):
+
+    @staticmethod
+    def send_response(code=status.HTTP_200_OK, err=None):
+        return Response(
+            {
+                "error": err,
+            },
+            status=code,
+        )
+
+    def post(self, request):
+        try:
+            # Get keystore
+            keystore = request.data["keystore"]
+
+            # Generate password hash
+            password = hashutils.hash_dict({
+                "password": request.data["password"],
+                "keystore": keystore,
+            })
+
+            # SELECT 1 FROM user;
+            is_user_exists = User.objects.filter(
+                identifier = keystore["pubKey"],
+                password = password,
+            )
+
+            # Validate: User found
+            if is_user_exists:
+                return self.send_response()
+
+            # Validate: User not found
+            return self.send_response(status.HTTP_401_UNAUTHORIZED, "User not found")
+
+        # Unknown error
+        except Exception as err:
+            return self.send_response(
+                status.HTTP_500_INTERNAL_SERVER_ERROR,
+                convutils.error_message(err),
+            )
