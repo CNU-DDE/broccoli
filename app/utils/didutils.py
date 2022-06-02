fd8936547675d665e63cb27c934bae16a04328d8
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-02 21:26:48 +0900

    fix: dict grammar error

--- app/utils/didutils.py
+++ app/utils/didutils.py
@@ -1,11 +1,11 @@
 import requests
 import json
 from django.conf import settings
-from .errors import DIDReqError
+from ..errors import DIDReqError
 
 def did_get_req(path, req_param=None):
-    res = requests.get(settings.DID_HOST + path, params=req_param)
+    res = requests.get("http://" + settings.DID_HOST + path, params=req_param)
     body = json.loads(res.text)
-    if res.status_code >= 400 or body.error is not None:
-        raise DIDReqError(res.status_code, body.error)
-    return body.content
+    if res.status_code >= 400 or body['error'] is not None:
+        raise DIDReqError(res.status_code, body['error'])
+    return body['content']
