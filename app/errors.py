5f6d69b9345edb0227bf873dae8189a24ee2529b
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-04 00:21:33 +0900

    fix: use err message again

--- app/errors.py
+++ app/errors.py
@@ -3,9 +3,11 @@ from rest_framework import status
 class DIDReqError(Exception):
     def __init__(self, status_code, message):
         self.status_code = status_code
+        self.message = message
         super().__init__(message)
 
 class JWTValidationError(Exception):
     def __init__(self, message) -> None:
         self.status_code = status.HTTP_401_UNAUTHORIZED
+        self.message = message
         super().__init__(message)
