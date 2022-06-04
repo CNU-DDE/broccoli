807fb132bc609b19209f316d2ccfc083af140c13
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-04 09:34:45 +0900

    fix PositionData table chema

--- app/models/position_data.py
+++ app/models/position_data.py
@@ -3,16 +3,12 @@ from .base_data import BaseData
 
 class PositionData(BaseData, models.Model):
 
-    employment_period = models.CharField(
-        max_length = 255,
-    )
+    employment_period = models.CharField(max_length = 255)
 
-    working_time = models.CharField(
-        max_length = 255,
-    )
+    working_time = models.CharField(max_length = 255)
 
-    payment_interval_type = models.SmallIntegerField()
+    payment_interval_type = models.CharField(max_length=64)
 
-    payment_per_internal = models.IntegerField()
+    payment_per_interval = models.CharField(max_length=64)
 
     hiring_number = models.IntegerField()
