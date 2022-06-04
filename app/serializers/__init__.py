63b7b8898e05e3311c9d7eb863ef02f5c7eacd6c
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-04 16:23:39 +0900

    feat: ClaimData default serializer

--- app/serializers/__init__.py
+++ app/serializers/__init__.py
@@ -1,16 +1,26 @@
 from .user_serializer import UserSerializer
+
 from .CL_serializer import CLSerializer
 from .CL_list_serializer import CLListSerializer
+
 from .position_serializer import PositionSerializer
 from .position_list_serializer import PositionListSerializer
 from .position_detail_serializer import PositionDetailSerializer
+
 from .resume_serializer import ResumeSerializer
+
+from .claim_serializer import ClaimSerializer
 
 # Ignore unused
 _ = UserSerializer
+
 _ = CLSerializer
 _ = CLListSerializer
+
 _ = PositionSerializer
 _ = PositionListSerializer
 _ = PositionDetailSerializer
+
 _ = ResumeSerializer
+
+_ = ClaimSerializer
