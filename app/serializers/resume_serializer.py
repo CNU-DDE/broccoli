8b872a02cc021bae92e44483b10b1fe0bc6fa79f
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-05 19:54:59 +0900

    feat: resume detail serializer

--- app/serializers/resume_serializer.py
+++ app/serializers/resume_serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
-from ..models import ResumeData
-from .user_serializer import UserDisplayNameSerializer
+
+from . import user_serializer, CL_serializer
+from ..models import ResumeData, CLData
+from ..utils.httputils import did_post_req
 
 # Default serializer
 class ResumeSerializer(serializers.ModelSerializer):
@@ -24,9 +26,41 @@ class ResumeDisplaySerializer(serializers.ModelSerializer):
     # Recursive formatting
     def to_representation(self, obj):
         resp = super().to_representation(obj)
-        resp["holder"] = UserDisplayNameSerializer(obj.owner).data
+        resp["holder"] = user_serializer.UserDisplayNameSerializer(obj.owner).data
         if obj.verifier:
-            resp["verifier"] = UserDisplayNameSerializer(obj.verifier).data
+            resp["verifier"] = user_serializer.UserDisplayNameSerializer(obj.verifier).data
         else:
             resp["verifier"] = None
+        return resp
+
+class ResumeDetailSerializer(serializers.ModelSerializer):
+
+    #Serialized format
+    class Meta:
+        model = ResumeData
+        fields = [
+            "id",
+            "title",
+            "position",
+        ]
+
+    # Recursive formatting
+    def to_representation(self, obj):
+        resp = super().to_representation(obj)
+
+        # Format holder
+        resp["holder"] = user_serializer.EmployeeReadableSerializer(obj.owner).data
+
+        # Format verifier
+        if obj.verifier:
+            resp["verifier"] = user_serializer.EmployerReadableSerializer(obj.verifier).data
+        else:
+            resp["verifier"] = None
+
+        # Save cover letter list to process on view-level
+        resp["cover_letters"] = obj.content["cover-letters"]
+
+        # Save VP to process on view-level
+        resp["careers"] = obj.content["careers"]
+
         return resp
