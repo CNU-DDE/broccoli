fd12b1f8ab2a069a399bdc1a4c89b58f3ba3c6b8
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-05 13:53:05 +0900

    test: modify to load employee / employer request body form

--- curl_test/post_user_token.test.sh
+++ curl_test/post_user_token.test.sh
@@ -4,7 +4,7 @@ set -e
 
 # Remove all whitespace
 # Ref: https://www.baeldung.com/linux/remove-whitespace-from-file#2-removing-all-whitespace-characters-1
-TEST_BODY=$(sed ':a; N; s/[[:space:]]//g; ta' post_user_token.json)
+TEST_BODY=$(sed ':a; N; s/[[:space:]]//g; ta' ${1:-'employee'}_test_keystore.json)
 
 curl -d "$TEST_BODY" \
     -H "Content-Type: application/json" \
