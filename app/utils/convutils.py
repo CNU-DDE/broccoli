b9fff42d3d999268070eb12b492821c920b76b58
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-11 14:47:17 +0900

    feat: [GET] employers list

--- app/utils/convutils.py
+++ app/utils/convutils.py
@@ -1,8 +1,5 @@
-from django.conf import settings
-
 def user_type(is_employee):
-    runmode = 0 if settings.RUNMODE == "production" else 2
-    return runmode + (1 if is_employee else 0)
+    return (1 if is_employee else 0)
 
 def error_message(err):
     return f"Unexpected error {err} with error type {type(err)}"
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,5 +1,6 @@
 from .user_response                 import *
 from .user_token_response           import *
+from .user_employer_response        import *
 from .cover_letter_response         import *
 from .position_response             import *
 from .position_detail_response      import *
