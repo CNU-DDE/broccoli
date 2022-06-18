1ffc4d39210ac6ae305146295a8f985a15a86728
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-19 03:22:01 +0900

    feat: wating for db to start up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+for i in $(seq 1 5); do
+    echo "Waiting for MariaDB to start up... ($i)"
+    sleep 1
+done
+
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate app
